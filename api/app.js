fb0976f96ae4ae6e493070d72ac796bd4bad5b99
Henry Summers <ssumo89@gmail.com>
2023-06-02 12:45:10 +0100

    added header authorisation and metrics display

--- api/app.js
+++ api/app.js
@@ -27,6 +27,18 @@ app.use("/time", timeRouter);
 
 const promMid = require("express-prometheus-middleware");
 
+app.use((req, res, next) => {
+  const authHeader = req.header("authorization"),
+    approvedToken = "mysecrettoken";
+
+  if (authHeader === approvedToken) {
+    next();
+  } else {
+    res.status(403);
+    res.render("unauthorised");
+  }
+});
+
 // const PORT = 9000;
 app.use(
   promMid({
@@ -35,6 +47,7 @@ app.use(
     requestDurationBuckets: [0.1, 0.5, 1, 1.5],
     requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
     responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    prefix: "metric_",
   })
 );
 
